fbcfcc8e3ee6a2e2d3dabc173eefe80bc600658b
LviGexe <mateozz-01@hotmail.com>
2025-10-09 11:22:33 -0500

    fix: adaptar dependencias paraa Laravel y Snipe-IT.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,40 +2,42 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ImportLocations;
+use App\Console\Commands\ReEncodeCustomFieldNames;
+use App\Console\Commands\RestoreDeletedUsers;
+use App\Models\Setting;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
-     * Define los comandos Artisan personalizados.
-     *
-     * @var array
-     */
-    protected $commands = [
-        // Agrega aquí tus comandos personalizados
-    ];
-
-    /**
-     * Define la programación de tareas.
+     * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        if(Setting::getSettings()?->alerts_enabled === 1) {
+            $schedule->command('snipeit:inventory-alerts')->daily();
+            $schedule->command('snipeit:expiring-alerts')->daily();
+            $schedule->command('snipeit:expected-checkin')->daily();
+            $schedule->command('snipeit:upcoming-audits')->daily();
+        }
+        $schedule->command('snipeit:backup')->weekly();
+        $schedule->command('backup:clean')->daily();
+        $schedule->command('auth:clear-resets')->everyFifteenMinutes();
+        $schedule->command('saml:clear_expired_nonces')->weekly();
     }
 
     /**
-     * Registra los comandos para la aplicación.
-     *
-     * @return void
+     * This method is required by Laravel to handle any console routes
+     * that are defined in routes/console.php.
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
+        $this->load(__DIR__.'/Commands');
     }
 }
